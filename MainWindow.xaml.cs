e2785def2c1150462789cf26f4a9fd18b626ebc1
Tom <tomarrowtomarrow@hotmail.com>
2022-10-15 01:48:42 +0200

    idk, been too long

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
 
 namespace TrackingSRTGenerator
 {
@@ -23,6 +25,39 @@ namespace TrackingSRTGenerator
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BtnGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            double howManySeconds = double.Parse(length_txt.Text);
+            double stepSize = double.Parse(step_txt.Text);
+            string prefix = prefix_txt.Text;
+
+            string theFile = "";
+
+            int index = 1;
+            for(double seconds = 0; seconds<howManySeconds; seconds += stepSize)
+            {
+                TimeSpan timeFormatter = TimeSpan.FromSeconds(seconds);
+                TimeSpan timeFormatterPlusStep = TimeSpan.FromSeconds(seconds+stepSize);
+
+                theFile += index + "\n" + timeFormatter.Hours.ToString("00") + ":" + timeFormatter.Minutes.ToString("00") + ":" + timeFormatter.Seconds.ToString("00") + "," + timeFormatter.Milliseconds.ToString("000")
+                    + " --> "
+                    + timeFormatterPlusStep.Hours.ToString("00") + ":" + timeFormatterPlusStep.Minutes.ToString("00") + ":" + timeFormatterPlusStep.Seconds.ToString("00") + "," + timeFormatterPlusStep.Milliseconds.ToString("000")
+                    + "\n"
+                    + prefix + timeFormatter.Hours.ToString("00") + ":" + timeFormatter.Minutes.ToString("00") + ":" + timeFormatter.Seconds.ToString("00") + ":" + (timeFormatter.Milliseconds/10).ToString("00")
+                    + "\n\n";
+                index++;
+            }
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "SubRip subtitle file (.srt)|*.srt";
+            sfd.FileName = "tracker.srt";
+            if(sfd.ShowDialog() == true)
+            {
+                File.WriteAllText(sfd.FileName, theFile);
+                MessageBox.Show("done.");
+            }
         }
     }
 }
